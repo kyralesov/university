930e0192b1e7967cac9bdc4e45d31c9a5b452528
Yura <Yura@air-yura.dlink>
2016-05-02 23:25:52 +0300

    Initial Commit

---
+++ university/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  university
+//
+//  Created by Yura on 02.05.16.
+//  Copyright © 2016 Kyralesov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
